26e5ddf4f062ab2bff0518211afa9c8408502a66
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2024-03-07 09:56:15 -0600

    chore: update user-agent

--- src/freshdesk/client.py
+++ src/freshdesk/client.py
@@ -36,9 +36,11 @@ logger.setLevel(logging.DEBUG)
 
 class UnregisteredClient(BaseClient):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-        " AppleWebKit/537.36 (KHTML, like Gecko)"
-        " Chrome/93.0.4577.82 Safari/537.36"
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+            " AppleWebKit/537.36 (KHTML, like Gecko)"
+            " Chrome/122.0.0.0 Safari/537.36"
+        )
     }
     api = SimpleNamespace()
 
